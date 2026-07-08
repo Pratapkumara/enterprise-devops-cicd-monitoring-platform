d5be0b1aa34ea29f4df81921da1af6d629c93f18
Pratapkumara <pratapsahoo772@gmail.com>
2026-07-09 02:17:58 +0530

    Update HealthController.java

--- app/src/main/java/com/enterprise/devops/controller/HealthController.java
+++ app/src/main/java/com/enterprise/devops/controller/HealthController.java
@@ -1,25 +1,25 @@
 package com.enterprise.devops.controller;
 
 import com.enterprise.devops.dto.HealthResponse;
+import com.enterprise.devops.service.HealthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Instant;
-
 @RestController
 public class HealthController {
+
+    private final HealthService healthService;
+
+    public HealthController(HealthService healthService) {
+        this.healthService = healthService;
+    }
 
     @GetMapping("/api/v1/health")
     public ResponseEntity<HealthResponse> health() {
 
-        HealthResponse response = new HealthResponse(
-                "SUCCESS",
-                "enterprise-devops-cicd-monitoring-platform",
-                "1.0.0",
-                Instant.now()
+        return ResponseEntity.ok(
+                healthService.getHealthStatus()
         );
-
-        return ResponseEntity.ok(response);
     }
 }
