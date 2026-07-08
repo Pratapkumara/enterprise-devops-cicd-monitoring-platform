514f15b0e50f2b4f344dedf2bea029f5c9a5155b
Ubuntu <ubuntu@ip-172-31-12-151.ap-south-1.compute.internal>
2026-07-08 19:50:34 +0000

    Initial Spring Boot project bootstrap with Actuator and Prometheus

---
+++ app/src/main/java/com/enterprise/devops/EnterpriseDevOpsMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.enterprise.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseDevOpsMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseDevOpsMonitoringApplication.class, args);
+    }
+}
